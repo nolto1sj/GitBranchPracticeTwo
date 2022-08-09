1e1013e630ddbaf628bdd7b7947fe9655d972603
nolto1sj <sjnolton@gmail.com>
2022-08-08 21:34:39 -0400

    Added pet class

---
+++ Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchPracticeTwo
+{
+    internal abstract class Pet
+    {
+        public string Name { get; set; }
+        public virtual void MakeHappyNoise()
+        {
+            Console.WriteLine("caw caw!");
+        }
+    }
+}
